e2755b477ef4a3b10e35c46e35a09b10912afe7b
cjw <cjwcommuny@outlook.com>
2023-09-26 15:19:50 +0800

    feat: support basic functions (#1)
    Co-authored-by: Kermit <chenjiawei1@xiaohongshu.com>

---
+++ src/lib.rs
@@ -0,0 +1,62 @@
+use std::fmt::{Debug, Formatter};
+use std::marker::PhantomData;
+
+pub struct Id<I, T> {
+    id: I,
+    phantom: PhantomData<fn() -> T>,
+}
+
+impl<I, T> Id<I, T> {
+    pub fn new(id: I) -> Self {
+        Self {
+            id,
+            phantom: PhantomData,
+        }
+    }
+
+    pub fn inner(self) -> I {
+        self.id
+    }
+}
+
+impl<I, T> AsRef<I> for Id<I, T> {
+    fn as_ref(&self) -> &I {
+        &self.id
+    }
+}
+
+impl<I: Debug, T> Debug for Id<I, T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_tuple("Id").field(&self.id).finish()
+    }
+}
+
+impl<I: Clone, T> Clone for Id<I, T> {
+    fn clone(&self) -> Self {
+        Self {
+            id: self.id.clone(),
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<I: Copy, T> Copy for Id<I, T> {}
+
+impl<I, T> From<I> for Id<I, T> {
+    fn from(value: I) -> Self {
+        Self::new(value)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::Id;
+
+    trait IsSync: Sync {}
+
+    impl<I: Sync, T> IsSync for Id<I, T> {}
+
+    trait IsSend: Send {}
+
+    impl<I: Send, T> IsSend for Id<I, T> {}
+}
